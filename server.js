4fb29d44ca056dc847688fba4fdc6de2c67ce5fa
Prezlyata Yuriy <Prezlyata Yuriy>
2018-11-05 15:12:17 +0200

    commented manifest

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 //     res.sendFile(path.resolve(__dirname, 'build/index.js'));
 // });
 
-app.get('', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
